c2228003f0521504164bacdacfbccd6f05a026d0
Mohammad Pourhosein <m.pourhosein7777@example.com>
2025-04-21 11:43:52 +0330

    latest commit

---
+++ server/middlewares/userExist.js
@@ -0,0 +1,21 @@
+const jwt=require("jsonwebtoken");
+
+module.exports=function(req,res,next){
+    let token=req.cookies.auth_cookie;
+    if(!token){
+        token=req.headers.auth_cookie;
+    }
+    if(!token){
+        res.status(401).json({msg:"لطفا لاگین کنید...",router:"login"});
+    }else{
+        
+        try{
+            const verified=jwt.verify(token,process.env.TOKEN_SECRET);
+            req.user=verified;
+            next();
+        }catch(err){
+            console.log(err);
+            res.status(200).json({msg:"لطفا لاگین کنید.",router:"login"});
+        }
+    }
+}
